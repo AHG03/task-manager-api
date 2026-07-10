957b721533ccb2893f8e132d71c7332aa854279d
Ali Ahangar <aliahangar80@gmail.com>
2026-07-10 15:01:51 +0200

    Add GET endpoint for all tasks

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+tasks = {
+    1: {"id": 1, "title": "Grocery", "completed": False},
+    2: {"id": 2, "title": "Laundry", "completed": False},
+    3: {"id": 3, "title": "Clean Room", "completed": False}
+}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return list(tasks.values())
 
 
 @app.get("/")
